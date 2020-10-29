eb6aa18d9e609ee1c86bcaefacd777900d85f866
Vlad Anghel <vlad.anghel1@gmail.com>
2020-10-29 14:46:00 +0100

    set up sass

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Career Foundry
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CareerFoundry</h1>
+      <p>
+        Choose one course we have to offer
+      </p>
     </div>
   );
 }
