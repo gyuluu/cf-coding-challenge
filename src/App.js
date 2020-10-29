384c1e8778c63776f610792e35eb9d7455baefd1
Vlad Anghel <vlad.anghel1@gmail.com>
2020-10-29 15:44:48 +0100

    display menu on click

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './App.scss';
 
 function App() {
+  const [showMenu, setShowMenu] = useState(false);
 
   useEffect(() => {
     fetch("https://private-e05942-courses22.apiary-mock.com/courses")
@@ -12,17 +13,28 @@ function App() {
       .catch(err => console.log('this is your error: ', err));
   }, []);
 
+  const displayMenu = (e) => {
+    e.preventDefault();
+    setShowMenu(!showMenu);
+  }
+
   return (
     <div className='app'>
       <div className='navigation'>
         <h1 className="cf-logo">CareerFoundry</h1>
         <div className='dropdown'>
-          <button className="dropbtn">Courses ↓</button>
-          <div className='menu'>
-              <a href='#'> Course 1 </a>
-              <a href='#'> Course 2 </a>
-              <a href='#'> Course 3 </a>
-          </div>
+          <button className="dropbtn" onClick={displayMenu}>Courses ↓</button>
+          {
+            showMenu ? (
+              <div className='menu'>
+                <a href='#'> Course 1 </a>
+                <a href='#'> Course 2 </a>
+                <a href='#'> Course 3 </a>
+              </div>
+            ) : (
+              null
+            )
+          }
         </div>
       </div>
     </div>
