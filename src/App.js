4508b8c5afe7200372fb8aee33f81b38242de801
Vlad Anghel <vlad.anghel1@gmail.com>
2020-10-29 14:54:53 +0100

    get cf courses from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import React, {useEffect} from 'react';
 import './App.scss';
 
 function App() {
+
+  useEffect(() => {
+    fetch("https://private-e05942-courses22.apiary-mock.com/courses")
+      .then(response => response.json())
+      .then(json => {
+        console.log('these are your courses: ', json);
+      })
+      .catch(err => console.log('this is your error: ', err));
+  }, []);
+
   return (
     <div className="App">
       <h1>CareerFoundry</h1>
