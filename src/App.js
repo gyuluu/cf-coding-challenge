9e32ca8b875381109af6cc1ad5537539e62e98c3
Vlad Anghel <vlad.anghel1@gmail.com>
2020-10-29 16:57:39 +0100

    display course details and dates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import './App.scss';
 function App() {
   const [showMenu, setShowMenu] = useState(false);
   const [courses, setCourses] = useState([]);
+  const [showCourse, setShowCourse] = useState(false);
+  const [selectedCourse, setSelectedCourse] = useState({});
+  const [startDate, setStartDate] = useState('');
+  const [nextDates, setNextDates] = useState([]);
 
   useEffect(() => {
     fetch("https://private-e05942-courses22.apiary-mock.com/courses")
@@ -20,6 +24,22 @@ function App() {
     setShowMenu(!showMenu);
   }
 
+  const selectCourse = (e) => {
+    console.log("this is your course: ", e.target.textContent);
+    const filteredCourse = courses.filter(course => course.title === e.target.textContent);
+    fetch(`https://private-e05942-courses22.apiary-mock.com/courses/${filteredCourse[0].slug}`)
+      .then(response => response.json())
+      .then(json => {
+        console.log('this is your selected course: ', json);
+        setSelectedCourse(json);
+        setStartDate(json.start_dates[0]);
+        setNextDates(json.start_dates.slice(1));
+      })
+      .catch(err => console.log(err));
+    setShowCourse(true);
+    setShowMenu(false);
+  }
+
   return (
     <div className='app'>
       <div className='navigation'>
@@ -31,7 +51,7 @@ function App() {
               <div className='menu'>
                 {
                   courses.map((course, index) => 
-                    <a href='#' key={index}> {course.title} </a>
+                    <span key={index} onClick={selectCourse}>{course.title}</span>
                   )
                 }
               </div>
@@ -41,6 +61,22 @@ function App() {
           }
         </div>
       </div>
+      {
+        showCourse ? (
+          <div>
+            <p className='course-description'>{selectedCourse.description}</p>
+            <p><span className='course-date'>Next Start Date: </span>{startDate}</p>
+            <p><span className='course-date'>Next Dates: </span></p>
+            {
+              nextDates.map((date, index) => 
+                <p key={index}>{date}</p>
+              )
+            }
+          </div>
+        ) : (
+          null
+        )
+      } 
     </div>
   );
 }
